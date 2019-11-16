7ae727e912dd4ae8d9f39ada55e2e28772e78705
msymori <msy.mori.developer@gmail.com>
2019-11-16 00:29:50 +0000

    create city form

--- app/controllers/city_controller.rb
+++ app/controllers/city_controller.rb
@@ -1,2 +1,29 @@
+require "city_energy.rb"
+
 class CityController < ApplicationController
+  def index
+    @cities_name = City.all
+    @cities_energy = CityEnergy.sum_cities_energy()
+    @energy_count = CityEnergy.group('city_id').count
+  end
+  
+  def new
+    @city = City.new
+  end 
+  
+  def create
+    @city = City.new(city_params)
+    if @city.save
+      flash[:success] = 'Create City!!'
+      redirect_to root_url
+    else
+      flash.now[:danger] = 'Not Create City...'
+      render root_url
+    end
+  end
+  
+  
+  def city_params
+    params.require(:city).permit(:name)
+  end
 end
--- app/models/city_energy.rb
+++ app/models/city_energy.rb
@@ -1,8 +1,13 @@
 class CityEnergy < ApplicationRecord
   belongs_to :city
   
-  def sum_city_energy(id)
-    sum = group('city_id').sum('energy_production')
-    sum[id]
+  def self.sum_city_energy(id)
+    @sum = group('city_id').sum('energy_production')
+    @sum[id]
   end
+  
+  def self.sum_cities_energy()
+    @sum = group('city_id').sum('energy_production')
+  end
+
 end
